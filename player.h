610ae431ac83f5cec46f5539236bb78d1c5fe60d
Jiayuan Feng <47410423+JiayuanFengSkyler@users.noreply.github.com>
2020-11-22 02:46:39 +0800

    Add files via upload

---
+++ player.h
@@ -0,0 +1,32 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+#include "lifetracks.h"
+
+using std::string;
+using std::vector;
+
+class Player{
+private:
+    string name;
+    int gender;
+    lifetrack *cont;
+    lifetrack *root;
+    string getTrack();
+
+public:
+    Player(): name(""){
+        root = makeTrack();
+        cont = root;
+        cont->status = occured;
+    };
+
+    void giveName(string name_) { name = name_; };
+    void printTrack();
+    void selectTrack(unsigned int n);
+    void load(char *argv);
+	void save();
+};
+
+#endif
